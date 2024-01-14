8a3404fa17a2f30ffa0117d944a4a2238ff3785d
Rachel <racheljiang310@gmail.com>
2024-01-13 19:38:34 -0800

    adding nav

--- productivity-app/src/App.js
+++ productivity-app/src/App.js
@@ -1,69 +1,31 @@
-import WeatherWindow from './WeatherWindow';
-import React, { useState, useEffect } from 'react';
-import { googleLogout, useGoogleLogin } from '@react-oauth/google';
-import axios from 'axios';
+import Profile from './Profile';
+import Home from './Home';
+import Navigation from './Navigation';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
-    const [ user, setUser ] = useState([]);
-    const [ profile, setProfile ] = useState([]);
-
-    const login = useGoogleLogin({
-        onSuccess: (codeResponse) => setUser(codeResponse),
-        onError: (error) => console.log('Login Failed:', error)
-    });
-
-    useEffect(
-        () => {
-            if (user) {
-                axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                    headers: {
-                      Authorization: `Bearer ${user.access_token}`,
-                      Accept: 'application/json'
-                    }
-                })
-                .then((res) => {
-                    setProfile(res.data);
-                })
-                .catch((err) => console.log(err));
-            }
-        },
-        [ user ]
-    );
-
-    const logOut = () => {
-        googleLogout();
-        setProfile(null);
-    };
-
     return (
-        <div className="app-Header">
-          <header>
-            <h1>Personal Assistant Page</h1>
-            {profile ? (
-                <div className="login-bar">
-                    <div className="profile-item"><img className="profile-photo" src={profile.picture} alt="profile"/></div>
-                    <div className="profile-item"><h4>Profile</h4></div>
-                    <br />
-                    <div className="profile-item"><h4>Name: {profile.name}</h4></div>
-                    <div className="profile-item"><h4>Email: {profile.email}</h4></div>
-                    <div className="profile-item"><button onClick={logOut}>Log out</button></div>
-                </div>
-            ) : (
-                <button onClick={() => login()}>Google Sign-In</button>
-            )}
-            </header>
-            <body>
-              {profile ? (
-                  <div className="weather-app">
-                    <WeatherWindow/>
-                  </div>
-              ) : (
-                  <p>Please Login To Use Feature</p>
-              )}
-            </body>
-            
+      <Router>
+        <div>
+        <nav className="nav-bar">
+            <div><Link to="/">Home</Link></div>
+            <div><Link to="/profile">Profile</Link></div>
+            <div><Link to="/navigation">Navigation</Link></div>
+         </nav>
         </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/navigation" element={<Navigation />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     );
 }
 export default App;
---
+++ productivity-app/src/Navigation.js
@@ -0,0 +1,8 @@
+// import { useState, useEffect } from 'react';
+
+function Navigation(){
+    return(
+        <h1>Navigation Page</h1>
+    )
+}
+export default Navigation;
